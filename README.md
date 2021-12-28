1d83fcd92dccecafe3685ac69184757ee26b7f05
Rubylago <rubybibi6@gmail.com>
2021-12-28 22:23:02 +0800

    Initialize Project

---
+++ README.md
@@ -0,0 +1,41 @@
+# 尋找餐廳
+
+本專案提供特定餐廳資料搜尋
+
+## 基本功能
+
+。使用者可以在首頁看到所有餐廳與它們的簡單資料：
+餐廳照片、餐廳名稱、餐廳分類、餐廳評分
+
+。使用者可以再點進去看餐廳的詳細資訊：
+類別、地址、電話、描述、圖片
+
+。使用者可以透過搜尋餐廳名稱來找到特定的餐廳
+
+。使用者可以透過搜尋餐廳類別來找到特定的餐廳
+
+### Installing
+
+Clone the repo
+
+Open the project's root directory
+
+Use npm to install dependencies
+
+```
+npm install
+```
+
+Run the project
+
+```
+npm run dev
+```
+
+That's it, now the app should be running on http://localhost:3000
+
+## 使用工具
+
+Visual Studio Code - 開發環境
+Express - 應用程式架構
+Express-Handlebars - 模板引擎
---
+++ app.js
@@ -0,0 +1,44 @@
+// require express
+const express = require('express')
+const app = express()
+port = 3000
+// require express-handlebars
+const exphbs = require('express-handlebars')
+// require restaurants list
+const restaurants = require('./restaurant.json')
+
+//set template engine
+app.engine('handlebars', exphbs({defaultLayout:'main'}))
+app.set('view engine', 'handlebars')
+
+//告訴 Express 靜態檔案的資料夾位置
+app.use(express.static('public'))
+
+//routes setting
+app.get('/', (req, res)=>{
+  res.render('index', {restaurant: restaurants.results})
+})
+
+app.get('/restaurants/:restaurant_id', (req, res)=>{
+  // get restaurant_id
+  // console.log('restaurants id:', req.params.restaurant_id)
+
+  const restaurant = restaurants.results.find(restaurant =>
+    restaurant.id.toString() === req.params.restaurant_id
+  )
+  res.render('show', { restaurant: restaurant })
+})
+
+app.get('/search', (req, res)=>{
+  // get queryString
+  // console.log('query:', req.query.keyword)
+
+  const keyword = req.query.keyword
+  const restaurant = restaurants.results.filter(restaurant => restaurant.name.toLowerCase().includes(keyword.toLowerCase())).length>0 ? restaurants.results.filter(restaurant => restaurant.name.toLowerCase().includes(keyword.toLowerCase())) : restaurants.results.filter(restaurant => restaurant.category.toLowerCase().includes(keyword.toLowerCase()))
+
+  res.render('index', {restaurant: restaurant, keyword: keyword})
+})
+
+app.listen(port, ()=>(
+  console.log(`this app is listen to http://localhost:${port}`)
+))
