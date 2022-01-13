26fd16abfa38439aee4a0d1ec14e3d2c7d88a00e
Rubylago <rubybibi6@gmail.com>
2022-01-13 15:50:24 +0800

    feat: add mongoose & restaurant seeder

---
+++ models/seeds/restaurantSeeder.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+// 載入 Restaurant Schema Model
+const RestaurantSchema = require('../Restaurant')
+
+// 載入 restaurant.jason
+const restaurantList = require('../../restaurant.json')
+
+mongoose.connect('mongodb://localhost/restaurants-list')
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error')
+})
+
+//載入restaurant.json
+db.once('open', () => {
+  console.log('mongodb connecting')
+
+  RestaurantSchema.create(restaurantList)
+    .then(() => {
+      console.log('restaurant insert done')
+    })
+    .catch(error => console.log('error'))
+})
