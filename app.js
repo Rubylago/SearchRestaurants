0c27b97376b902135eff4e74dfe4d4ea77383e7d
Rubylago <rubybibi6@gmail.com>
2022-01-22 14:02:16 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,25 +1,14 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const { get, redirect } = require('express/lib/response')
-const mongoose = require('mongoose')
+
 const RestaurantSchema = require('./models/Restaurant')
 const methodOverride = require('method-override')
 const routes = require('./routes')
+require('./config/mongoose')
 
 const app = express()
 port = 3000
-
-mongoose.connect('mongodb://localhost/restaurants-list')
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected')
-})
 
 // set template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,23 +1,8 @@
-const mongoose = require('mongoose')
-
-// 載入 Restaurant Schema Model
 const RestaurantSchema = require('../Restaurant')
-
-// 載入 restaurant.jason
 const restaurantList = require('../../restaurant.json').results
+const db = require('../../config/mongoose')
 
-mongoose.connect('mongodb://localhost/restaurants-list')
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error')
-})
-
-//載入restaurant.json
 db.once('open', () => {
-  console.log('mongodb connecting')
-
   RestaurantSchema.create(restaurantList)
     .then(() => {
       console.log('restaurant insert done')
