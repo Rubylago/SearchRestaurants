1fab8bdd8abea3fa25df49edf33812961f7cc80a
Rubylago <rubybibi6@gmail.com>
2022-03-30 12:44:46 +0800

    feat: add user login page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -5,4 +5,14 @@ router.get('/login', (req, res) => {
   res.render('login')
 })
 
+router.post('/login', (req, res) => {
+  // login
+})
+
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+
+
 module.exports = router
