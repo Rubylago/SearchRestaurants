6259aca7366cbd875920ce6606cb2a08cc01c8d0
Rubylago <rubybibi6@gmail.com>
2022-03-30 14:17:51 +0800

    feat: add user register feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const UserSchema = require('../../models/user')
 
 router.get('/login', (req, res) => {
   res.render('login')
@@ -11,6 +12,28 @@ router.post('/login', (req, res) => {
 
 router.get('/register', (req, res) => {
   res.render('register')
+})
+
+router.post('/register', (req, res) => {
+  // 取得註冊表單內容
+  const { name, email, password, confirmPassword } = req.body
+  // 判斷是否已經註冊  
+  UserSchema.findOne({ email })
+    .then(user => {
+      // 如果已經註冊 => 返回原本頁面並跳提醒文字
+      if(user) {
+        console.log('已經註冊')
+        res.render('register', {
+          name, email, password, confirmPassword
+        })
+      } else{
+        // 如果還沒註冊 => User create 寫入資料庫
+        return UserSchema.create({name, email, password})
+          .then(() => res.redirect('/'))
+          .catch(error => console.log(error))
+      }
+    })
+    .catch(error => console.log(error))  
 })
 
 
